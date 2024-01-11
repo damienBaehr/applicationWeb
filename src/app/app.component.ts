f7687e82a5cca8d643f42239c6a95815d9b181e8
Damien Baehr <damien.baehr@hotmail.com>
2024-01-11 17:13:35 +0100

    call api + http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { SendMessageComponent } from './send-message.component';
+import { HttpClient } from '@angular/common/http';
 
 
 // // ---------------BASE
@@ -59,19 +60,62 @@ import { SendMessageComponent } from './send-message.component';
 
 
 // ---------------EX 3 - Bind d'évènement'
+interface Message {
+  username : string;
+  content: string;
+}
+
+interface DadJoke {
+  id: string;
+  joke: string;
+  status : number;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, SendMessageComponent],
-  template: `<send-message (send)="send($event)"/>`,
+  imports: [FormsModule, SendMessageComponent, CommonModule],
+  template: `
+  @if(logged){
+    <send-message (send)="send($event)"/>
+  }@else { 
+    <input class="border border-slate-600 p-2 m-2" type="text" [(ngModel)]="username">
+    <button (click)="login()">Connexion</button>
+  }
+  <button (click)='getDadJoke()'>getDadJoke</button>
+  @for (item of messages; track $index){
+    <div class="p-4 rounded m-4" [ngClass]="item.username === 'dad' ? 'bg-slate-400 ml-16' : 'bg-slate-100' ">
+      <p class="font-bold">{{ item.username }}</p>
+      <p>{{ item.content }}</p>
+    </div>
+  }
+  `,
 })
 export class AppComponent {
   
-  messages : string[] = [] ;
+  messages : Message[] = [] ;
+  logged : boolean = false;
+  username = "";
+  http = inject(HttpClient);
 
   send(message : string){
-    this.messages.push(message)
+    if(this.username){
+      this.messages.push({username : this.username, content : message})
+    }
     console.log("Messages envoyés",this.messages)
+  }
+
+  login(){
+    if(this.username){
+      this.logged = true;
+    }
+  }
+
+  getDadJoke(){
+    this.http.get<DadJoke>("https://icanhazdadjoke.com/", {headers : {"Accept": "application/json"}}).subscribe(e => {
+      let message : Message = {username : "dad", content: e.joke}
+      this.messages.push(message)
+    })
   }
     
 }
--- src/app/send-message.component.ts
+++ src/app/send-message.component.ts
@@ -1,29 +1,34 @@
-import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
 @Component({
-    selector:"send-message",
-    standalone: true,
-    imports:[FormsModule],
-    template : `
-    <div class="bg-slate-300 p-10 flex gap-4 w-full">
-    <input [(ngModel)]="message" class="p-3 rounded-md bg-slate-50" type="text"/>
-    <button (click)="send.emit(message)" class=" px-5 rounded-md bg-slate-100">SEND</button>
-  </div>
-  `
+    selector : "send-message",
+    standalone : true,
+    imports: [
+        FormsModule
+    ],
+    template : 
+        `<div class="bg-slate-100 p-4 flex gap-2 items-center">
+            <input type="text" [ngModel]="message" (ngModelChange)="message = $event" class="rounded border-slate-200 p-2"/>
+            <button (click)="_send()" class="ml-2">Envoyer</button>
+        </div>`
+    
 })
-
 export class SendMessageComponent {
-    message :string  = "";
+
+    message = "";
 
     @Output()
-    send = new EventEmitter();
+    send = new EventEmitter<string>();
 
-    constructor(){
-        this.send.subscribe(() => {
+    protected _send() {
+
+        if (!this.message) {
+            alert("YA PAS DE MESSAGE ");
             this.message = "";
-        })
+        } else {
+            this.send.emit(this.message);
+            this.message = "";
+        }
     }
-
-   
 }
